d71626e01e0cdf90b5caae76fdf1375968399ea8
Daniel Gutierrez Calzada <danigutcal02@gmail.com>
2020-10-26 09:22:36 +0100

    Ahora el sol aparece lentamente, ligeramente por encima del campo

--- Picture.java
+++ Picture.java
@@ -15,6 +15,7 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
+    private Square campo;
 
     /**
      * Constructor for objects of class Picture
@@ -48,12 +49,25 @@ public class Picture
         roof.moveVertical(-60);
         roof.makeVisible();
 
+        
+        
+        
+        
+        campo = new Square();
+        campo.changeColor("green");
+        campo.moveHorizontal(-400);
+        campo.moveVertical(140);
+        campo.changeSize(373737);
+        campo.makeVisible();
+        
         sun = new Circle();
         sun.changeColor("yellow");
         sun.moveHorizontal(100);
-        sun.moveVertical(-40);
+        sun.moveVertical(105);
         sun.changeSize(80);
         sun.makeVisible();
+        sun.slowMoveVertical(-185);
+        
     }
 
     /**
